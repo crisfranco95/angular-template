faf63afea4eaabf269a3658ac4b886af3634160d
Cristian Franco <cristianjunior22@hotmail.com>
2022-07-25 11:07:22 -0500

    Bump angular flex layour cdk and material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,11 @@ import { appRoutes } from './routing/routing';
 import { HomeComponent } from './home/home.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list'
 import { ExternalRegisterComponent } from './external-register/external-register.component';
 import { ForgottenPasswordComponent } from './forgotten-password/forgotten-password.component';
 import { HomeDashboardComponent } from './home-dashboard/home-dashboard.component';
--- src/app/material-components-definition/material-components-definition.module.ts
+++ src/app/material-components-definition/material-components-definition.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule,
-  MatCheckboxModule,
-  MatCardModule,
-  MatTableModule,
-  MatInputModule,
-  MatTooltipModule,
-  MatDialogModule,
-  MatProgressBarModule,
-  MatToolbarModule,
-  MatSidenavModule,
-  MatListModule,
-  MatGridListModule,
-  MatTabsModule,
-  MatMenuModule,
-  MatSelectModule,
-  MatPaginatorModule,
-  MatIconModule} from '@angular/material';
 
+import { MatButtonModule } from '@angular/material/button'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatCardModule } from '@angular/material/card'
+import { MatTableModule } from '@angular/material/table'
+import { MatInputModule } from '@angular/material/input'
+import { MatTooltipModule } from '@angular/material/tooltip'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatProgressBarModule } from '@angular/material/progress-bar'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatTabsModule } from '@angular/material/tabs'
+import { MatMenuModule } from '@angular/material/menu'
+import { MatSelectModule } from '@angular/material/select'
+import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
   imports: [
